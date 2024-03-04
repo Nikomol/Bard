fdd93e3a7ee519d3cc9ce068e3515acece6fe9c5
Angelo <da.create.gg@gmail.com>
2024-03-05 00:56:22 +0700

    add something.... interesting

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import './App.css';
 
 
 export default function App() {
   const [content, setContent] = useState(1);
 
-  function handleClick(){
+  const [text_content, setTextContent] = useState("Привет, мир!");
+
+  function handleClick() {
     setContent(content + 1);
+    if(content >= 10 && content < 20){
+      setTextContent(`Ух ты!! + ${content}см твоей пипиське!!!`);
+    }
+    else if(content >= 20){
+      setTextContent(`А вот хер тебе!, остальные ${content}см пойдут к моей пипиське :)`);
+    }
   }
 
   return (
     <>
-      <button onClick={handleClick}>{content}</button>
+      <div className='startPage'>
+        <h1>{text_content}</h1>
+        <div className='magicDiv'>
+          <button onClick={handleClick} className='magicButton'>Нажми на меня: {content}</button>
+        </div>
+      </div>
     </>
   )
 }
