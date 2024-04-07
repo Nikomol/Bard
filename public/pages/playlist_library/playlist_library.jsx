2a5064583e0c717789020a4763a8a95ad7495194
Angelo <da.create.gg@gmail.com>
2024-04-07 20:23:27 +0700

    Всё сломал, ничего не сделал(бля как так?)

--- public/pages/playlist_library/playlist_library.jsx
+++ public/pages/playlist_library/playlist_library.jsx
@@ -1,3 +1,44 @@
-export default function CreatePLaylist(){
-    
+import { useState, useEffect } from "react";
+
+export default function CreatePLaylist() {
+    const [playlist, setPlaylist] = useState([]);
+    const [check, setCheck] = useState([]);
+
+    //логика отправки get/post запроса
+    ///.......///
+
+    const createDiv = (playlistTitle, playlistAuthor, playlistUrl) => {
+        return (
+            <>
+                <button className="librarby playlist">
+                    <h1 className="library playlist title">
+                        {playlistTitle}
+                    </h1>
+                    <h2 className="library playlist author">
+                        {playlistAuthor}
+                    </h2>
+                </button>
+            </>
+        );
+    }
+
+    const checkPlaylist = () => {
+        console.log("Playlist: create");
+        for(let i = 0; i < 10; i++){
+            setCheck(
+                ...check,
+                createDiv("some title", "some author", "some Url")
+            );
+        }
+        console.log(`Playslist: ${check}`);
+
+        return(<div></div>);
+    }
+
+    return(
+        <>
+            {checkPlaylist}
+        </>
+    );
+
 }
