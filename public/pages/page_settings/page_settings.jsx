78e4d6e9b7e41dc837f2955745d3bc6b8c2bed05
Angelo <da.create.gg@gmail.com>
2024-04-15 01:06:52 +0700

    Создал div элементы с настройками

--- public/pages/page_settings/page_settings.jsx
+++ public/pages/page_settings/page_settings.jsx
@@ -1,9 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+
 import './page_settings.scss'
 
 export default function PageSettings(){
+
+    const navigate = useNavigate();
+
+    const NavigateToProfile = () =>{
+        navigate('/profile');
+    }
+
     return(
         <>
-            
+            <div className=''>
+                {/*Нужно сделать текст выключеной конпки более тёмнын(все кнопки, кроме кнопки "Профиль" - выключены. Чекай disabled)*/}
+                <button className='' onClick={NavigateToProfile}>Профиль</button> {/*Кнопка включена*/}
+                <button className='' disabled={true}>Настройки</button> {/*Кнопка выключена*/}
+                <button className='' disabled={true}>Сменить аккаунт</button> {/*Кнопка выключена*/}
+                <button className='' disabled={true}>Выйти</button> {/*Кнопка выключена*/}
+            </div>
         </>
     );
 }
