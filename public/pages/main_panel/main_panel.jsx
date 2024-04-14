ff30d63f9b8d6b1eb24ab283dd916801f11d1fb9
Angelo <da.create.gg@gmail.com>
2024-04-15 03:21:47 +0700

    Начал менять логику входа пользователя на сервер.

--- public/pages/main_panel/main_panel.jsx
+++ public/pages/main_panel/main_panel.jsx
@@ -1,3 +1,19 @@
-export default function MainPanel(){
-    
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+
+import './main_panel.scss';
+
+export default function MainPanel(user){
+    const [isLogged, setIsLogged] = useState(false);
+
+    return(
+        <>
+            {'login' in user && 'id' in user ? 
+                <div className="main-backdrop">
+                    
+                </div> 
+                : <Navigate to={"/login"} /> 
+            }
+        </>
+    );
 }
