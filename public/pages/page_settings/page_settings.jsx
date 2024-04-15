cb4037349d1a0633d01a80293a3b16277e5e513f
Angelo <da.create.gg@gmail.com>
2024-04-15 16:05:35 +0700

    Немного переписал стили

--- public/pages/page_settings/page_settings.jsx
+++ public/pages/page_settings/page_settings.jsx
@@ -1,18 +1,38 @@
 import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 import './page_settings.scss'
 
-export default function PageSettings(){
+export default function PageSettings() {
+
+    const [searchContainer, setSearchContainer] = useState();
 
     const navigate = useNavigate();
 
-    const NavigateToProfile = () =>{
+    const NavigateToProfile = () => {
         navigate('/profile');
     }
 
-    return(
+    useEffect(() => {
+        const handleResize = () => {
+            const search = document.querySelector('.upper_panel');
+            setSearchContainer(search.clientHeight);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        handleResize();
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    const panelHeight = {
+        top: `calc(${searchContainer}px + 16px)`
+    };
+
+    return (
         <>
-            <div className='Profile'>
+            <div className='Profile' style={panelHeight}>
                 {/*Нужно сделать текст выключеной конпки более тёмнын(все кнопки, кроме кнопки "Профиль" - выключены. Чекай disabled)*/}
                 <button className='Profile-Button Enable Upper' onClick={NavigateToProfile}>Профиль</button> {/*Кнопка включена*/}
                 <button className='Profile-Button Disable Middle' disabled={true}>Настройки</button> {/*Кнопка выключена*/}
